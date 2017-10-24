4df98b08d12796c1876fcec292d2501e1698afb2
Bourgoin Théo <theo.bourgoin@hetic.net>
2017-10-24 23:28:56 +0200

    remove useless php file

--- src/Blog/views/index.php
+++
@@ -1,6 +0,0 @@
-<?= $renderer->render('header') ?>
-<h1>Blog</h1>
-<ul>
-    <li><a href="<?= $router->generateUri('blog.show', ['slug' => 'first-post']) ?>">First post</a></li>
-</ul>
-<?= $renderer->render('footer') ?>
--- src/Blog/views/show.php
+++
@@ -1,5 +0,0 @@
-<?= $renderer->render('header', ['title' => $slug]); ?>
-
-<h1>Post <?= $slug ?></h1>
-
-<?= $renderer->render('footer'); ?>
